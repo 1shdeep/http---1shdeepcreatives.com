61359824d7408d9e4a0bf1177623770f206b9d3c
1shdeep <yashansh15@gmail.com>
2024-05-12 13:25:35 +0530

    updated backend again

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 // db.js
-const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables from .env
-
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env
+console.log(process.env.DB_URL)
 const connectDB = async () => {
   try {
     // await mongoose.connect(process.env.DB_URL, {
-    await mongoose.connect(process.env.DB_URI, {
+      await mongoose.connect("mongodb://0.0.0.0:27017/shdeep_project", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error('MongoDB connection error:', error);
   }
 };
 

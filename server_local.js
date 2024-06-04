36a8c25aa57e96d62cfd20fd585da9c27329b603
Sanjay Kumar <sk04852@gmail.com>
2024-06-05 00:43:02 +0530

    update

---
+++ server_local.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const portfolioRouter = require('./routes/portfolio');
+const testimonialsRouter = require('./routes/testimonials');
+const clientRouter = require('./routes/client');
+const adminRouter = require('./routes/admin');
+const { createUser } = require("./migrations");
+const connectDB = require('./db');
+const cors = require('cors'); 
+require('dotenv').config(); 
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+connectDB(); 
+app.use(express.json());
+
+// Configure CORS middleware
+app.use(cors()); // Allow all CORS requests by default
+
+app.use('/api/portfolio', portfolioRouter);
+app.use('/api/testimonials', testimonialsRouter);
+app.use('/api/client', clientRouter);
+app.use('/api/admin', adminRouter);
+
+async function connect() {
+  try {
+    createUser();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Start the server
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on port ${port}`);
+});
